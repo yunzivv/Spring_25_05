43210a76899b38d895d46e5ba8a03604f8035e69
yunzivv <yunzivv@gmail.com>
2025-05-20 15:07:45 +0900

    resultData.from data2 추가

--- src/main/java/com/example/demo/vo/ResultData.java
+++ src/main/java/com/example/demo/vo/ResultData.java
@@ -36,7 +36,24 @@ public class ResultData<DT> {
 
 		return rd;
 	}
+	
+	public static <DT> ResultData<DT> from(String resultCode, String msg, String data1Name, DT data1, String data2Name,
+			DT data2) {
+		ResultData<DT> rd = new ResultData<DT>();
+		rd.ResultCode = resultCode;
+		rd.msg = msg;
+		rd.data1Name = data1Name;
+		rd.data1 = data1;
+		rd.data2Name = data2Name;
+		rd.data2 = data2;
 
+		return rd;
+	}
+
+	public static <DT> ResultData<DT> newData(ResultData rd, String dataName, DT newData) {
+		return from(rd.getResultCode(), rd.getMsg(), dataName, newData);
+	}
+	
 	public boolean isSuccess() {
 		return ResultCode.startsWith("S-");
 	}
@@ -45,7 +62,4 @@ public class ResultData<DT> {
 		return isSuccess() == false;
 	}
 
-	public static <DT> ResultData<DT> newData(ResultData rd, String dataName, DT newData) {
-		return from(rd.getResultCode(), rd.getMsg(), dataName, newData);
-	}
 }
